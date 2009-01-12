faab2dfcb931c1b34172b5b030f5c7bf6a2c86aa
Michal Wallace <michal.wallace@gmail.com>
2009-01-12 14:00:30 +0000

    removed where junk from saftey lookups to get tests passing
    git-svn-id: file:///svn/svn/cornerops@74 fd6e30d5-8636-0410-b3ec-e778ce416229

--- cornerhost/trunk/cornerhost/safety.py
+++ cornerhost/trunk/cornerhost/safety.py
@@ -3,7 +3,7 @@ from storage import where
 from cornerhost import User, Domain, DNSRec, EmailRule, Mailbox, Site, Database
 
 
-def only(series, cond):
+def only(series, left, right):
     """
     Fetches one item from a series.
 
@@ -13,12 +13,12 @@ def only(series, cond):
     be a general purpose thing so I'll leave the
     more complex code in for now.
     """
-    assert str(cond.operation) == "="
-    result = [x for x in series if getattr(x, cond.left.name) == cond.right]
+    #assert str(cond.operation) == "=", cond.operation
+    result = [x for x in series if getattr(x, left.name) == right]
 
     # error reporting:
     klass = series.type.__name__
-    where = "where %s %s %s" % (cond.left.name, cond.operation, cond.right)
+    where = "where %s %s %s" % (left.name, '=', right)
     if len(result) ==  0: raise LookupError("no %s found %s" % (klass, where))
     if len(result) > 1: raise LookupError("found more than one %s %s" % (klass, where))
 
@@ -27,25 +27,25 @@ def only(series, cond):
 # and here's the payoff :)
     
 def safeDomain(user, domName):
-    return only(user.domains,  where(Domain.domain) == domName)
+    return only(user.domains,  Domain.domain, domName)
 
 def safeEmailRule(domain, virtuser):
-    return only(domain.rules, where(EmailRule.virtuser) == virtuser)
+    return only(domain.rules, EmailRule.virtuser, virtuser)
 
 def safeEmailRuleByID(domain, ID):
-    return only(domain.rules, where(EmailRule.ID) == int(ID))
+    return only(domain.rules, EmailRule.ID, int(ID))
 
 def safeMailbox(user, mailbox):
-    return only(user.boxes, where(Mailbox.mailbox) == mailbox)
+    return only(user.boxes, Mailbox.mailbox, mailbox)
 
 def safeSite(user, ID):
-    return only(user.sites, where(Site.ID) == int(ID))
+    return only(user.sites, Site.ID, int(ID))
 
 def safeSiteByName(user, domName):
     return safeDomain(user, domName).site
 
 def safeDNSRec(domain, ID):
-    return only(domain.dnsrecs, where(DNSRec.ID) == int(ID))
+    return only(domain.dnsrecs, DNSRec.ID, int(ID))
 
 def safeDb(user, dbname):
-    return only(user.dbs, where(Database.dbname)==dbname)
+    return only(user.dbs, Database.dbname, dbname)
